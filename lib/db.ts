174e4bb7b48792fe56a01dc1ffcfee3e8f887f8c
amittriyar <amittryl@gmail.com>
2026-02-19 06:47:39 +0530

    Refactor database connection to use global variable

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-let prisma: PrismaClient | null = null;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export function getDb() {
-  if (!prisma) {
-    prisma = new PrismaClient();
+  if (!globalForPrisma.prisma) {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    globalForPrisma.prisma = new PrismaClient({
+      adapter,
+    });
   }
-  return prisma;
+
+  return globalForPrisma.prisma;
 }
